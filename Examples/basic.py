50c5d5a3774a7d0ccfe14664738ebbbe4417ac10
bgockeln <bengockeln@gmail.com>
2024-08-30 15:12:12 +0200

    hmm

---
+++ Examples/basic.py
@@ -0,0 +1,17 @@
+#This is a comment
+"""
+this
+is a multiline
+comment
+"""
+dayInYear = 365 #This is a Variable
+myMsg = "You idiot" #This is a String Variable
+numA = 10
+numB = 20
+print(dayInYear)
+print(myMsg)
+#Simple Math
+print(3+3)
+print(3*3)
+print(numA * numB)
+print("First line. \nSecond Line.")
